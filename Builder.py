9eb4f568b49077b546b625b095e4b709c7186f54
vovahrytsiuk <vovaoffonua@gmail.com>
2021-12-27 18:55:07 +0200

    own requests builder

--- Builder.py
+++ Builder.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import Any
+from SingletonDB import DB
 
 
 class Builder(ABC):
     @property
     @abstractmethod
-    def request(self) -> None:
+    def requests(self) -> None:
         pass
 
     @abstractmethod
@@ -14,11 +15,7 @@ class Builder(ABC):
         pass
 
     @abstractmethod
-    def get_by_filtered_params(self) -> None:
-        pass
-
-    @abstractmethod
-    def produce_part_c(self) -> None:
+    def filter_requests(self) -> None:
         pass
 
 
@@ -30,30 +27,37 @@ class OwnRequestsBuilder(Builder):
         self._requests = OwnRequests()
 
     @property
-    def product(self) -> OwnRequests:
-        product = self._product
+    def requests(self) -> OwnRequests:
+        requests = self._requests
         self.reset()
-        return product
+        return requests
 
-    def produce_part_a(self) -> None:
-        self._product.add("PartA1")
+    def get_from_source(self) -> None:
+        self._requests.set_requests(DB().get_data())
 
-    def produce_part_b(self) -> None:
-        self._product.add("PartB1")
-
-    def produce_part_c(self) -> None:
-        self._product.add("PartC1")
+    def filter_requests(self) -> None:
+        pass
 
 
-class OwnRequests():
+class OwnRequests:
     def __init__(self) -> None:
-        self.parts = []
+        self.requests = []
 
     def add(self, part: Any) -> None:
         self.parts.append(part)
 
-    def list_parts(self) -> None:
-        print(f"Product parts: {', '.join(self.parts)}", end="")
+    def merge(self, other_requests):
+        self.requests += other_requests.requests
+
+    def delete_request(self, id):
+        del self.requests[id]
+
+    def set_requests(self, new_requests):
+        self.requests = new_requests
+
+    def delete(self, id):
+        pass
+        # put here call to db to delete requests with id = id
 
 
 class Director:
@@ -68,10 +72,9 @@ class Director:
     def builder(self, builder: Builder) -> None:
         self._builder = builder
 
-    def build_minimal_viable_product(self) -> None:
-        self.builder.produce_part_a()
+    def build_all_requests(self) -> None:
+        self.builder.get_from_source()
 
-    def build_full_featured_product(self) -> None:
-        self.builder.produce_part_a()
-        self.builder.produce_part_b()
-        self.builder.produce_part_c()
+    def build_filtered_requests(self) -> None:
+        self.builder.get_from_source()
+        self.builder.filter_requests()
